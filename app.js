910633b1ce2b6c1b73415bb01da5c331cc720550
em.klokov <grafinfector@hotmail.com>
2017-02-04 08:45:47 +0500

    Перспектива

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var gl;
 var vertextShaderCode = [
 'precision mediump float;',
 '',
-'attribute vec2 position;',	
+'attribute vec3 position;',
 'attribute float vertParam;',
 'varying float param;',
 '',
+'uniform mat4 mWorld;',
+'uniform mat4 mView;',
+'uniform mat4 mProj;',
+'',
 'void main(void) {',
 '    param = vertParam;',
-'    gl_Position = vec4(position, 0.0,  1.0);',
+'    gl_Position = mProj * mView * mWorld * vec4(position, 1.0);',
 '}'
 ].join("\n");
 
@@ -24,110 +28,133 @@ var pixelShaderCode = [
 ].join("\n");
 
 
-var initEngine = function() {
-   gl = document.getElementById("canvas").getContext("webgl");
-   if(!gl) {
-      gl = document.getElementById("canvas").getContext("experimental-webgl");
-   }
+var initEngine = function () {
+    gl = document.getElementById("canvas").getContext("webgl");
+    if (!gl) {
+        gl = document.getElementById("canvas").getContext("experimental-webgl");
+    }
 
-   gl.clearColor(0.5, 0.75, 0.85, 1);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clearColor(0.5, 0.75, 0.85, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-   var 
-      vs = createVertextShader(vertextShaderCode),
-      ps = createPixelShader(pixelShaderCode),
-      program = createProgram();
+    var
+        vs = createVertextShader(vertextShaderCode),
+        ps = createPixelShader(pixelShaderCode),
+        program = createProgram();
 
-   program.attachShaders(vs, ps);
+    program.attachShaders(vs, ps);
 
-   gl.linkProgram(program.prog);
-   if(!gl.getProgramParameter(program.prog, gl.LINK_STATUS)) {
-      console.error('Error: link program');
-      return;
-   }
+    gl.linkProgram(program.prog);
+    if (!gl.getProgramParameter(program.prog, gl.LINK_STATUS)) {
+        console.error('Error: link program');
+        return;
+    }
 
-   gl.validateProgram(program.prog);
-   if(!gl.getProgramParameter(program.prog, gl.VALIDATE_STATUS)) {
-      console.error('Error: validate program');
-      return;
-   }
+    gl.validateProgram(program.prog);
+    if (!gl.getProgramParameter(program.prog, gl.VALIDATE_STATUS)) {
+        console.error('Error: validate program');
+        return;
+    }
 
-   // Init geometry;
-   var vertexes = new Float32Array([
-   // x     y     x     p
-     -0.5,  0.5,  0.0,  1.0,
-     -0.5, -0.5,  0.0,  0.0,
-      0.5, -0.5,  0.0, -1.0,
+    // Init geometry;
+    var vertexes = new Float32Array([
+    // x     y     x     p
+        -0.5, 0.5, 0.0, 1.0,
+        -0.5, -0.5, 0.0, 0.0,
+        0.5, -0.5, 0.0, -1.0,
 
-      0.5,  0.5,  0.0,  1.0,
-     -0.5,  0.5,  0.0,  0.0,
-      0.5, -0.5,  0.0, -1.0      
-   ])
+        0.5, 0.5, 0.0, 1.0,
+        -0.5, 0.5, 0.0, 0.0,
+        0.5, -0.5, 0.0, -1.0
+    ]);
 
-   // shader layout
-   var vertexesBufferObject = gl.createBuffer();
-   gl.bindBuffer(gl.ARRAY_BUFFER, vertexesBufferObject);
-   gl.bufferData(gl.ARRAY_BUFFER, vertexes, gl.STATIC_DRAW);
+    // shader layout
+    var vertexesBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexesBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexes, gl.STATIC_DRAW);
 
-   var posAttrLocation = gl.getAttribLocation(program.prog, 'position');   
-   gl.vertexAttribPointer(
-      posAttrLocation, // положение атрибута.
-      3,
-      gl.FLOAT,
-      gl.FALSE,
-      4 * Float32Array.BYTES_PER_ELEMENT,
-      0
-   );
+    var posAttrLocation = gl.getAttribLocation(program.prog, 'position');
+    gl.vertexAttribPointer(
+        posAttrLocation, // положение атрибута.
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        4 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
 
-   var paramAttrLocation = gl.getAttribLocation(program.prog, 'vertParam');
-   gl.vertexAttribPointer(
-      paramAttrLocation, // положение атрибута.
-      1,
-      gl.FLOAT,
-      gl.FALSE,
-      4 * Float32Array.BYTES_PER_ELEMENT,
-      3 * Float32Array.BYTES_PER_ELEMENT
-   );   
+    var paramAttrLocation = gl.getAttribLocation(program.prog, 'vertParam');
+    gl.vertexAttribPointer(
+        paramAttrLocation, // положение атрибута.
+        1,
+        gl.FLOAT,
+        gl.FALSE,
+        4 * Float32Array.BYTES_PER_ELEMENT,
+        3 * Float32Array.BYTES_PER_ELEMENT
+    );
 
-   gl.enableVertexAttribArray(posAttrLocation);
-   gl.enableVertexAttribArray(paramAttrLocation);
-   
-   gl.useProgram(program.prog);
-   gl.drawArrays(gl.TRIANGLES, 0, 6);   
+    gl.enableVertexAttribArray(posAttrLocation);
+    gl.enableVertexAttribArray(paramAttrLocation);
+
+    gl.useProgram(program.prog);
+    var 
+        mWorldLocation = gl.getUniformLocation(program.prog, 'mWorld'),
+        mViewLocation = gl.getUniformLocation(program.prog, 'mView'),
+        mProjLocation = gl.getUniformLocation(program.prog, 'mProj'),
+
+        worldMatrix = new Float32Array(16),
+        viewMatrix = new Float32Array(16),
+        projMatrix = new Float32Array(16);
+
+    mat4.identity(worldMatrix);
+    mat4.lookAt(viewMatrix, [1, 0, -5], [0, 0, 0], [0, 1, 0]);
+    mat4.perspective(
+        projMatrix,
+        glMatrix.toRadian(45),
+        800 / 600,
+        0.1,
+        1000.0
+    );
+
+    gl.uniformMatrix4fv(mWorldLocation, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(mViewLocation, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(mProjLocation, gl.FALSE, projMatrix);
+    
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
 
 function createVertextShader(shaderCode) {
-   var shader = gl.createShader(gl.VERTEX_SHADER);
-   gl.shaderSource(shader, shaderCode);
-   gl.compileShader(shader);
-   if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.error('Error: compile vertext shader', gl.getShaderInfoLog(shader));
-      return null;
-   }
-   return shader;
+    var shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(shader, shaderCode);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('Error: compile vertext shader', gl.getShaderInfoLog(shader));
+        return null;
+    }
+    return shader;
 }
 
 function createPixelShader(shaderCode) {
-   var shader = gl.createShader(gl.FRAGMENT_SHADER);
-   gl.shaderSource(shader, shaderCode);
-   gl.compileShader(shader);   
-   if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.error('Error: compile pixel shader', gl.getShaderInfoLog(shader));
-      return null;
-   }
-   return shader;
+    var shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(shader, shaderCode);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error('Error: compile pixel shader', gl.getShaderInfoLog(shader));
+        return null;
+    }
+    return shader;
 }
 
 function createProgram() {
-   var   
-      program = gl.createProgram(),
-      f1 = function(vertexShader, pixelShader) { 
-         gl.attachShader(this, vertexShader);
-         gl.attachShader(this, pixelShader);
-      };
+    var
+        program = gl.createProgram(),
+        f1 = function (vertexShader, pixelShader) {
+            gl.attachShader(this, vertexShader);
+            gl.attachShader(this, pixelShader);
+        };
 
-   return {
-      prog: program,
-      attachShaders: f1.bind(program)
-   };
+    return {
+        prog: program,
+        attachShaders: f1.bind(program)
+    };
 }
--- gl-objects.js
+++ gl-objects.js
@@ -1,20 +1,20 @@
-function createDumpCube(width, height, depth) {
-   var cube = {};
+function createDumpCube(width, height, depth) {    
+    var cube = {};
 
-   cube.size = 32;
-   cube.vertices = new Float32Array(
-      [
-         // лицо
-         // x     y     x     p
-         -0.5,  0.5, -0.5,  1.0,
-         -0.5, -0.5, -0.5,  0.0,
-          0.5, -0.5, -0.5, -1.0,
+    cube.size = 6;
+    cube.vertices = new Float32Array(
+        [
+            // лицо
+            // pos (x,y,x), normal (xyz), param(f)
+            -(width * 0.5), (height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, 1.0,
+            -(width * 0.5), -(height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, 0.0,
+            (width * 0.5), -(height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, -1.0,
 
-          0.5,  0.5, -0.5,  1.0,
-         -0.5,  0.5, -0.5,  0.0,
-          0.5, -0.5, -0.5, -1.0 
-      ]
-   );
+            -(width * 0.5), (height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, 0.0,
+            (width * 0.5), -(height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, -1.0,
+            (width * 0.5), (height * 0.5), -(depth * 0.5), 0.0, 0.0, -1.0, 1.0
+        ]
+    );
 
-   return cube;
+    return cube;
 }
